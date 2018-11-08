2d37bf793520df647fb7b951bf8ccfacd6437813
Brandon Muhlestein <nd4spdoffroad@gmail.com>
2018-11-08 01:52:02 -0700

    Added "AddPassengerForm" to solution.

---
+++ CS3280_Assignment6_Part1/AddPassengerForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS3280_Assignment6_Part1
+{
+    public partial class AddPassengerForm : Form
+    {
+        public AddPassengerForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
